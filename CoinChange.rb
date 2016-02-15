c005cc29a634810594576d877129faf1eb85a327
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-02-15 11:07:57 -0600

    tests working now

--- CoinChange.rb
+++ CoinChange.rb
@@ -5,7 +5,41 @@ class CoinChange
   end
 
   def coin
-    return 1
+    if @amount == 0
+      return 0
+    elsif @amount >= 25
+      quarter
+    elsif @amount >= 10
+      dime
+    elsif @amount >= 5
+      nickel
+    else
+      return @amount.to_i
+    end
+  end
+
+  def quarter
+    if @amount % 25 == 0
+      return (@amount / 25)
+    else
+    return 1 + CoinChange.new(@amount - 25).coin
+    end
+  end
+
+  def dime
+    if @amount % 10 == 0
+      return (@amount / 10)
+    else
+      return 1 + CoinChange.new(@amount - 10).coin
+    end
+  end
+
+  def nickel
+    if @amount % 5 == 0
+      return (@amount / 5)
+    else
+      return 1 + CoinChange.new(@amount - 5).coin
+    end
   end
 
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -1,8 +1,25 @@
-require_relative '../CoinChange' #require_relative instead?
+require_relative '../CoinChange'
 
 describe CoinChange do
 
-  it "should return 1 coin for 25 cents"
+  it "should return 1 coin for 25 cents" do
     expect(CoinChange.new(25).coin).to be == 1
-  #end
+  end
+
+  it "should return 0 coins for 0 cents" do
+    expect(CoinChange.new(0).coin).to be == 0
+  end
+
+  it "should return 4 coins for 4 cents" do
+    expect(CoinChange.new(4).coin).to be == 4
+  end
+
+  it "should return 3 coins for 40 cents" do
+    expect(CoinChange.new(40).coin).to be == 3
+  end
+
+  it "should return 6 coins for 78 cents" do
+    expect(CoinChange.new(78).coin).to be == 6
+  end
+
 end
